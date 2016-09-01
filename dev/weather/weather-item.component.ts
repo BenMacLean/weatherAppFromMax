1ad269160a2ac394e56a7a21d08c43427ac54b31
BenMacLean <benhmaclean@gmail.com>
2016-09-01 13:26:14 -0300

    degrees c

--- dev/weather/weather-item.component.ts
+++ dev/weather/weather-item.component.ts
@@ -7,10 +7,10 @@ import {WeatherItem} from "./weather-item";
         <article class="weather-element">
             <div class="col-1">
                 <h3>{{ weatherItem.cityName }}</h3>
-                <p class="info">CLOUDS</p>
+                <p class="info">{{weatherItem.description}}</p>
             </div>
             <div class="col-2">
-                <span class="temperature">32°C</span>
+                <span class="temperature">{{weatherItem.temperature}}˚C</span>
             </div>
         </article>
     `,
--- dev/weather/weather-item.ts
+++ dev/weather/weather-item.ts
@@ -1,3 +1,3 @@
 export class WeatherItem{
-  constructor(public cityName: string, public description: string, publicTemperature: number){}
+  constructor(public cityName: string, public description: string, public temperature: number){}
 }
