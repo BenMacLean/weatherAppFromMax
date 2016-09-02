313ff22bd67a523bdf8429cd05ba9e46a635884d
BenMacLean <benhmaclean@gmail.com>
2016-09-02 14:53:23 -0300

    api key in

--- dev/weather/weather.service.ts
+++ dev/weather/weather.service.ts
@@ -1,8 +1,18 @@
 import {Injectable} from "angular2/core";
 import {WEATHER_ITEMS} from "./weather.data";
+import {Observable} from "rxjs/Observable";
+import {Http} from "angular2/http";
+import 'rxjs/Rx';
 @Injectable()
 export class WeatherService{
+
+  constructor (private _http: Http) {}
+
   getWeatherItems(){
     return WEATHER_ITEMS;
   }
+  searchWeatherData(cityName: string): Observable<any>{
+    return this._http.get('api.openweathermap.org/data/2.5/weather?q=' + cityName + 'APPID=90a335d103bc0c34332a9b97640be0ec&metric');
+  }
+
 }
