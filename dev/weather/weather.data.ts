6e01b74f470431d8b0aa5595cac045f965d98d16
BenMacLean <benhmaclean@gmail.com>
2016-09-09 20:47:52 -0300

    test commit

--- dev/weather/weather.data.ts
+++ dev/weather/weather.data.ts
@@ -1,5 +1,5 @@
 import {WeatherItem} from "./weather-item";
 export const WEATHER_ITEMS: WeatherItem[] = [
-  new WeatherItem('London', 'RAINY', 6),
-  new WeatherItem('New York', 'CLOUDY', 10)
+  // new WeatherItem('London', 'RAINY', 6),
+  // new WeatherItem('New York', 'CLOUDY', 10)
 ];
