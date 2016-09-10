e5cfa522d5e5639df8c0c02d9050016f3ff4b988
BenMacLean <benhmaclean@gmail.com>
2016-09-10 20:50:11 -0300

    add button

--- dev/sidebar.component.ts
+++ dev/sidebar.component.ts
@@ -2,7 +2,8 @@ import {Component} from "angular2/core";
 @Component({
   selector: 'my-sidebar',
   template: `
-
+      <h3?Saved Profiles </h3>
+      <button>Save List to Profile</button>
   `
 
 })
