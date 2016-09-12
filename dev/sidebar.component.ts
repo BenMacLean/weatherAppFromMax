c9064521a068759a6aabf09af40b0339790d07a2
BenMacLean <benhmaclean@gmail.com>
2016-09-12 14:40:09 -0300

    fixing backtick

--- dev/sidebar.component.ts
+++ dev/sidebar.component.ts
@@ -2,14 +2,15 @@ import {Component} from "angular2/core";
 @Component({
   selector: 'my-sidebar',
   template: `
-      <h3?Saved Profiles </h3>
+      <h3>Saved Profiles </h3>
       <button (click)="onSaveNew()">Save List to Profile</button>
-  `   <article>
-        <h4>Profile Name</h4>
-        <p>Cities: New York, London</p>
-        <span class="delete" (click)="onDeleteProfile($event)>x</span>
-      </article>
+      <article>
+           <h4>Profile Name</h4>
+           <p>Cities: New York, London</p>
+           <span class="delete" (click)="onDeleteProfile($event)">X</span>
+         </article>
 
+  `
 })
 
 export class SidebarComponent {
