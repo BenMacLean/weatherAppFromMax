41b24aa9304d2d6b4ea692cd0f7fb4d8305af74f
BenMacLean <benhmaclean@gmail.com>
2016-09-15 14:27:35 -0300

    Reverting to the state of the project

--- dev/boot.ts
+++ dev/boot.ts
@@ -2,6 +2,7 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from "./app.component";
 import {HTTP_PROVIDERS} from "angular2/http";
+import {WeatherService} from "./weather/weather.service";
 
 
-bootstrap(AppComponent, [HTTP_PROVIDERS]);
+bootstrap(AppComponent, [HTTP_PROVIDERS, WeatherService]);
